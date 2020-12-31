451b49d2b32ca54725e546806bfa0e83a12157dc
Fabrice Niessen <fniessen@pirilampo.org>
2020-12-31 17:26:51 +0100

    Add Lisp file for using Koma from Org mode

---
+++ lisp/org-latex-classes.el
@@ -0,0 +1,20 @@
+;;; org-latex-classes.el --- Sample configuration file for LaTeX
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'ox-latex)
+
+(add-to-list 'org-latex-classes
+             '("koma-article"
+               "\\documentclass{scrartcl}
+               [NO-DEFAULT-PACKAGES]
+               [EXTRA]"
+               ("\\section{%s}" . "\\section*{%s}")
+               ("\\subsection{%s}" . "\\subsection*{%s}")
+               ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+               ("\\paragraph{%s}" . "\\paragraph*{%s}")
+               ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
+
+;;; org-latex-classes.el ends here
